d874b75248d116fd1f7a0580aed1c26e02478a31
Yehouda Cohen <coheny748@gmail.com>
2026-03-11 14:26:33 +0200

    add adminOnly in route get report/:id

--- server/src/routes/reportRoutes.js
+++ server/src/routes/reportRoutes.js
@@ -4,6 +4,7 @@ import auth, { protect } from '../middleware/authMiddleware.js';
 import upload from '../middleware/upload.js';
 import { createReport, getReportById, getReports, importReportsCsv } from '../controllers/reportController.js';
 import { csvUpload } from '../middleware/multerCsv.js';
+import restrictToAdmin from '../middleware/adminMiddleware.js';
 
 const router = express.Router();
 
@@ -11,7 +12,7 @@ const router = express.Router();
 router.post('/', auth, upload.single('image'),createReport)
 
 router.get('/', protect,getReports) 
-router.get('/:id',protect,getReportById)
+router.get('/:id',protect,restrictToAdmin, getReportById)
 router.post('/csv',protect,csvUpload.single('file'),importReportsCsv)
 
 export default router;
